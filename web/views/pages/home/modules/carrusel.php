4fa0a0c25383724f8b52ce94bbe55ec3a1a1d2a0
raocampo <rao.ocampo@yahoo.com>
2025-07-16 22:40:29 -0500

    Se termina de crear el carrusel(slider) para PC

--- web/views/pages/home/modules/carrusel.php
+++ web/views/pages/home/modules/carrusel.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="<?php echo $path ?>views/assets/css/slide/slide.css">
+
+
 <div class="jd-slider">
 
     <div class="slide-inner">
@@ -6,10 +9,66 @@
 
             <li>
                 <img src="<?php echo $path ?>views/assets/img/carrusel/1/back_default.jpg" alt="img-fluid">
+
+                <div class="slideOpt opt1">
+
+                    <img src="<?php echo $path ?>views/assets/img/carrusel/1/calzado.png" style="top:15%; right:10%; width:45%">
+
+                    <div class="slideText" style="top: 20%; left:10%; width:45%;">
+
+                        <h1 class="text-uppercase" style="color:#333">Lorem Ipsum</h1>
+
+                        <h2 class="text-uppercase" style="color:#777">Lorem ipsum dolor sit</h2>
+
+                        <h3 class="text-uppercase" style="color:#888">Lorem ipsum dolor sit</h3>
+
+                        <a href="#">
+
+                            <button class="my-lg-3 btn text-uppercase templateColor border-0">
+
+                                VER PRODUCTO
+
+                            </button>
+
+                        </a>
+
+                    </div>
+
+
+                </div>
+
             </li>
 
             <li>
                 <img src="<?php echo $path ?>views/assets/img/carrusel/2/fondo2.jpg" alt="img-fluid">
+
+                <div class="slideOpt opt2">
+
+                    <img src="<?php echo $path ?>views/assets/img/carrusel/2/iphone.png" style="bottom:0%; left:15%; width:28%">
+
+                    <div class="slideText" style="top:20%; right:15%; width:40%">
+
+                        <h1 class="text-uppercase" style="color:#fff">Lorem Ipsum</h1>
+
+                        <h2 class="text-uppercase" style="color:#fff">Lorem ipsum dolor sit</h2>
+
+                        <h3 class="text-uppercase" style="color:#fff">Lorem ipsum dolor sit</h3>
+
+                        <a href="#">
+
+                            <button class="my-lg-3 btn text-uppercase templateColor border-0">
+
+                                VER PRODUCTO
+
+                            </button>
+
+                        </a>
+
+                    </div>
+
+
+                </div>
+
             </li>
 
             <li>
@@ -27,13 +86,13 @@
 
     <a class="prev" href="#">
 
-        <i class="fas fa-angle-left fa-2x"></i>
+        <i class="fas fa-angle-left text-white px-3 py-5 rounded-right" style="background: rgba(0, 0, 0, .5);"></i>
 
     </a>
         
-    <a class="next" href="">
+    <a class="next" href="#">
 
-        <i class="fas fa-angle-right fa-2x"></i>
+        <i class="fas fa-angle-right text-white px-3 py-5 rounded-left" style="background: rgba(0, 0, 0, .5);"></i>
 
     </a>
 
@@ -44,6 +103,4 @@
 
 </div>
 
-<script>
-    $('.jd-slider').jdSlider();
-</script>
+<script src="<?php echo $path ?>views/assets/js/slide/slide.js"></script>
